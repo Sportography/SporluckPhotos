de24cd0f1db0b963b7e4c852ea8018dedfd4065b
Sportography <sporluckphotos@gmail.com>
2025-06-17 22:50:23 -0500

    Fixed(?) main photo
    Added also tilt effect

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
 
   elements.forEach(el => observer.observe(el));
 });
+// 3D Tilt Effect on impact photo
+const tiltCard = document.getElementById("tilt-photo");
+
+if (tiltCard) {
+  tiltCard.addEventListener("mousemove", (e) => {
+    const rect = tiltCard.getBoundingClientRect();
+    const x = e.clientX - rect.left - rect.width / 2;
+    const y = e.clientY - rect.top - rect.height / 2;
+
+    tiltCard.style.transform = `rotateY(${x / 30}deg) rotateX(${-y / 30}deg)`;
+  });
+
+  tiltCard.addEventListener("mouseleave", () => {
+    tiltCard.style.transform = "rotateY(0deg) rotateX(0deg)";
+  });
+}
